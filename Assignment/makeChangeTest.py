9a6e9bf5c55dde5a437de91a9e2e3ffe15f8d797
umphress <umphress@eng.auburn.edu>
2020-01-29 10:00:22 -0600

    Assignment set up

---
+++ Assignment/makeChangeTest.py
@@ -0,0 +1,13 @@
+import unittest
+import Assignment.makeChange as mc
+
+class MakeChangeTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testName(self):
+        pass
