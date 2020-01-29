84cabd53bed2e2a46efde998d71895dd7bcb7c4f
umphress <umphress@eng.auburn.edu>
2020-01-29 09:44:17 -0600

    End of class commit

--- Fizzbuzz/fizzbuzz.py
+++ Fizzbuzz/fizzbuzz.py
@@ -1,2 +1,6 @@
-def fizzbuzz():
+def fizzbuzz(n = None):
+    if(n == None):
+        return "Danger, Will Robinson"
+    if(not(isinstance(n, int))):
+        return "Danger, Will Robinson"      
     pass
